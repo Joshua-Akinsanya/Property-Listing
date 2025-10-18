d2a0d5a7ca2ee298752a2d9fc561b074da44a803
Joshua Akinsanya <ojdaraakinsanya@gmail.com>
2025-10-18 12:42:16 +0100

    feat: add location filter functionality

--- src/components/PropertyMenu.jsx
+++ src/components/PropertyMenu.jsx
@@ -1,59 +1,84 @@
 import { useState, useEffect } from 'react'
 import { useFetchData } from '../hooks/useFetchData'
 import { DisplayProperties } from "./index"
-import CheckboxInput from './CheckboxInput'
 
 export default function PropertyMenu() {
 	const [propertyList, setPropertyList] = useState([]) // All data
-	const [filteredList, setFilteredList] = useState([]) // Filterable data to be displayed
-	const [availableLocations, setAvailableLocations] = useState([])
+	const [filteredPropertyList, setfilteredPropertyList] = useState([]) // Filterable data to be displayed
+
 	// Filtering States
+	const [locationOptions, setLocationOptions] = useState([]) // Options are set dynamically in a UseEffect to avoid hard coding the values
+	const [selectedLocations, setSelectedLocations] = useState([])
 	const [onlySuperhost, setOnlySuperhost] = useState(false)
 	const [bedroomSize, setBedroomSize] = useState(0)
-  const data = useFetchData()
+  
+	const data = useFetchData()
 
   useEffect(() => {
-    
 		setPropertyList(data)
-		setFilteredList(propertyList)
+		setfilteredPropertyList(propertyList)
 
 		// Checkbox Input will use this array for filter options
+		// Location Filter Options are set dynamically to avoid hard coding the values
 		if(data.length > 0){
 			const locationsArray = data.map(property => property.location) // This array may contain duplicates
-			const uniqueLocations = new Set(locationsArray)
-			setAvailableLocations([...uniqueLocations])
+			setLocationOptions(() => ["All Stays", ...locationsArray])
+			setLocationOptions((prev) => new Set(prev))
+			setLocationOptions((prev) => [...prev])
 		}
   }, [data])
+
+	const handleSelect = (selectedOption) => {
+		if( !selectedLocations.includes(selectedOption) ) {
+			setSelectedLocations([...selectedLocations, selectedOption])
+		}
+		else {
+			const temp = selectedLocations.filter(location => location !== selectedOption)
+			setSelectedLocations(temp)
+		}
+	}
 
 	// Filtering
 	useEffect(() => {
 		if(propertyList.length > 0) {
 			// Reset
-			setFilteredList(propertyList)
-			if(onlySuperhost) {
-				setFilteredList((prevArray) => prevArray.filter(item => item.superhost === true))
+			setfilteredPropertyList(propertyList)
+
+			// Filter by locations
+			if( selectedLocations.length > 0 && !selectedLocations.includes('All Stays') ) {
+				const tempProperties = propertyList.filter(property => selectedLocations.includes(property.location) )
+				setfilteredPropertyList(tempProperties)
 			}
+
+			// Filter by Superhost
+			if(onlySuperhost) {
+				setfilteredPropertyList((prevArray) => prevArray.filter(item => item.superhost === true))
+			}
+
+			// Filter by bedroom size
 			if(bedroomSize != 0) {
-				setFilteredList((prevArray) => prevArray.filter(item => item.capacity.bedroom == bedroomSize))
+				setfilteredPropertyList((prevArray) => prevArray.filter(item => item.capacity.bedroom == bedroomSize))
 			}
 		}
-	}, [onlySuperhost, bedroomSize])
+	}, [selectedLocations, onlySuperhost, bedroomSize])
 
 	return (
 		<>
-			{propertyList.length > 0 
+			{propertyList.length > 0
 				?<>
 						<div className="mx-[8.33%] mb-10">
 							<search className="bg-darkblue-opf2 border-1 border-gray-op40 rounded-xl py-8 px-10 flex justify-between flex-wrap -translate-y-4 lg:-translate-y-1/2">
 								<menu className="flex gap-3 flex-wrap">
-									<li><CheckboxInput name="All Stays" groupName="locations" /></li>
-									{
-										availableLocations.map(location => (
-											<li key={location}>
-												<CheckboxInput key={location} name={location} groupName="locations" />
-											</li>
-										))
-									}
+									{locationOptions.map( option => {
+										return (
+											<button className={`${selectedLocations.includes(option) && 'bg-darkgray'} cursor-pointer inline-block px-3 py-2 rounded hover:bg-gray-500 transition`}
+												key={option}
+												onClick={() => handleSelect(option)}
+											>
+												{option}
+											</button>
+										)
+									} )}
 								</menu>
 
 								<div className="space-x-5">
@@ -67,7 +92,6 @@ export default function PropertyMenu() {
 									<select name="property" id=""
 										className="border-solid border-1 rounded-lg px-6 py-3"
 										value={bedroomSize}
-										defaultValue={bedroomSize}
 										onChange={(e) => setBedroomSize(Number(e.target.value))}
 									>
 										<option value="0">All</option>
@@ -77,7 +101,7 @@ export default function PropertyMenu() {
 								</div>
 							</search>
 						</div>
-						<DisplayProperties propertyList={filteredList} />
+						<DisplayProperties propertyList={filteredPropertyList} />
 					</>
 				:
 					<span className='block mt-12 mx-auto w-5/6 text-center text-red-500 text-3xl'>An Error occurred !</span>
